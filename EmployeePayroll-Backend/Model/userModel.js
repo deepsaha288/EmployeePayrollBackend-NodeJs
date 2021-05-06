ce497d0f8879c7c1d96152b275942f8a44137a78
Deep Saha <deepsaha288@yahoo.com>
2021-05-06 17:09:51 +0530

    post method implement and model-schema and validator

--- EmployeePayroll-Backend/Model/userModel.js
+++ EmployeePayroll-Backend/Model/userModel.js
@@ -1,23 +1,42 @@
 const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
 
-    firstName: {
-        type: String
+    firstname: {
+        type: String,
+        required: true
     },
-    lastName: {
-        type: String
+    lastname: {
+        type: String,
+        required: true
     },
-    email: {
-        type: String
-    }
-}, {
-    timestamps: true
+    gender:{
+        type: String,
+        enum:['male','female']
+    },
+    emailId: {
+        type: String,
+        unique: true
+    },
+    phoneNumber: {
+        type: Number,
+        required: true
+    },
+    salary: {
+        type: Number,
+        required: true
+    },
+    department: {
+        type: String,
+        enum: ['HR', 'Sales', 'Finance', 'Engineer', 'Others'],
+        message: '{VALUE} is not supported'
+    },
+        timestamps:true
 });
 let users = mongoose.model('users', userSchema);
 
 class UserModel {
     userCreate(obj, callback) {
-         users.create(obj, (error, success) => {
+        users.create(obj, (error, success) => {
             if (error) {
                 callback(error)
             } else {
@@ -26,7 +45,7 @@ class UserModel {
         })
     }
 
-    retrieveData( callback){
+    retrieveData(callback) {
         users.find((error, success) => {
             if (error) {
                 callback(error)
