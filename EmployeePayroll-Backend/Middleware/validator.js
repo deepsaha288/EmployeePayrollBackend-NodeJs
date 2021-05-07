11a16751431828b377dd6c84901b1c5b69104638
Deep Saha <deepsaha288@yahoo.com>
2021-05-07 11:56:44 +0530

    added Conttroller for Get and Post updated file

--- EmployeePayroll-Backend/Middleware/validator.js
+++ EmployeePayroll-Backend/Middleware/validator.js
@@ -1,20 +1,31 @@
 let { body, validateResult } = require('express-validator');
+let response = {}
 module.exports = {
     register: [
-        body('firstname').toString.require,
-        body('lastname').toString,
-        body('emailId').toString,
-        body('phoneNumber').toString,
-        body('salary').toString,
-        body('department').toString
+        body('firstname').toString(),
+        body('lastname').toString(),
+        body('emailId').toString(),
+        body('phoneNumber').toString(),
+        body('salary').toString(),
+        body('department').toString()
     ]
 }
 
 module.exports.validateResult = (req, res,next) => {
+   try {
     const errors = validateResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
+        response.success = false;
+            response.message = "Invalid Credentials!";
+            response.error = errors.array();
+            return res.status(422).send(response);
     }else{
         next()
     }
+   } catch (error) {
+
+       response.success = false;
+        response.message = "Something went wrong!"
+        return res.status(500).send(response);
+   }
 }
